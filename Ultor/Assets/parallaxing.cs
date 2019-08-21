ab127ddb5696e138f70aee60cf2b1566481a2e2d
lomeli-noe <neolomeli@gmail.com>
2019-08-20 23:19:49 -0700

    Added parallaxing

---
+++ Ultor/Assets/parallaxing.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class parallaxing : MonoBehaviour
+{
+    public Transform[] backgrounds;
+    private float[] parallaxScales;
+    public float smoothing = 1; // Make sure to set this above 0.
+    private Transform cam; // Reference main camera's transform
+    private Vector3 previousCamPos; // Position of camera in previous frame
+
+    private void Awake()
+    {
+        cam = Camera.main.transform;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        previousCamPos = cam.position;
+        parallaxScales = new float[backgrounds.Length];
+
+        for(int i = 0; i < backgrounds.Length; i++)
+        {
+            parallaxScales[i] = backgrounds[i].position.z * -1;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for(int i = 0; i < backgrounds.Length; i++)
+        {
+            float parallax = (previousCamPos.x - cam.position.x) * parallaxScales[i];
+
+            float backgroundTargetPosX = backgrounds[i].position.x + parallax;
+
+            Vector3 backgroundTargetPos = new Vector3(backgroundTargetPosX, backgrounds[i].position.y, backgrounds[i].position.z);
+
+            backgrounds[i].position = Vector3.Lerp(backgrounds[i].position, backgroundTargetPos, smoothing * Time.deltaTime);
+        }
+
+        previousCamPos = cam.position;
+    }
+}
