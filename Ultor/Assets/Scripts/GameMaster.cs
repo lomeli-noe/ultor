469bcc5d0824cbc988a576913abd435f69ccdef8
lomeli-noe <neolomeli@gmail.com>
2019-09-07 22:51:26 -0700

    Add respawn audio

--- Ultor/Assets/Scripts/GameMaster.cs
+++ Ultor/Assets/Scripts/GameMaster.cs
@@ -18,9 +18,12 @@ public class GameMaster : MonoBehaviour
     public Transform spawnPoint;
     public int spawnDelay = 2;
     public Transform spawnPrefab;
+    public AudioClip respawnAudio;
 
     public IEnumerator RespawnPlayer()
     {
+
+        AudioSource.PlayClipAtPoint(respawnAudio, new Vector3(spawnPoint.position.x, spawnPoint.position.y, spawnPoint.position.z), 0.5f);
         yield return new WaitForSeconds(spawnDelay);
         Instantiate(playerPrefab, spawnPoint.position, spawnPoint.rotation);
         Transform clone = Instantiate(spawnPrefab, spawnPoint.position, spawnPoint.rotation) as Transform;
