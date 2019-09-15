a96a3638c16f115b1f7c17efa39c5c70627fff78
lomeli-noe <neolomeli@gmail.com>
2019-09-14 20:00:16 -0700

    Player health bar

--- Ultor/Assets/Scripts/Player.cs
+++ Ultor/Assets/Scripts/Player.cs
@@ -7,11 +7,39 @@ public class Player : MonoBehaviour
     [System.Serializable]
     public class PlayerStats
     {
-        public int Health = 100;
+        public int maxHealth = 100;
+
+		private int _curHealth;
+
+        public int curHealth
+		{
+			get { return _curHealth; }
+			set { _curHealth = Mathf.Clamp(value, 0, maxHealth); }
+		}
+
+        public void Init()
+		{
+			curHealth = maxHealth;
+		}
     }
 
-    public PlayerStats playerStats = new PlayerStats();
+    public PlayerStats stats = new PlayerStats();
+
     public int fallBoundary = -20;
+
+    [Header("Optional: ")]
+	[SerializeField]
+	private StatusIndicator statusIndicator;
+
+    void Start()
+	{
+		stats.Init();
+
+        if(statusIndicator != null)
+		{
+			statusIndicator.SetHealth(stats.curHealth, stats.maxHealth);
+		}
+	}
 
     private void Update()
     {
@@ -21,14 +49,23 @@ public class Player : MonoBehaviour
         }
     }
 
+
+
     public void DamagePlayer(int damage)
     {
-        playerStats.Health -= damage;
-        if(playerStats.Health <= 0)
+        stats.curHealth -= damage;
+		Debug.Log("curhealth: " + stats.curHealth);
+        if(stats.curHealth <= 0)
         {
             GameMaster.KillPlayer(this);
-        }
-    }
+		}
+
+		if (statusIndicator != null)
+		{
+			statusIndicator.SetHealth(stats.curHealth, stats.maxHealth);
+		} 
+
+	}
     
 
 }
